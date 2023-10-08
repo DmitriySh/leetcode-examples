dea2a7184bff20fe7a74ed52f2cd0cdd61c1610e
DmitriySh <dashishmakov@gmail.com>
2023-10-08 21:33:23 +0300

    Add JavDoc for ValidParentheses

--- src/main/java/ru/shishmakov/problems/ValidParentheses.java
+++ src/main/java/ru/shishmakov/problems/ValidParentheses.java
@@ -3,6 +3,15 @@ package ru.shishmakov.problems;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * <ul>
+ *     <li>open brackets must be closed by the same type of brackets;</li>
+ *     <li>open brackets must be closed in the correct order.</li>
+ * </ul>
+ * <a href="https://leetcode.ca/2015-12-20-20-Valid-Parentheses/">Valid parentheses: problem solution</a>
+ */
 public class ValidParentheses implements Runnable {
     public static final String DEFAULT_PARENTHESES = "()[{}]";
 
@@ -39,7 +48,7 @@ public class ValidParentheses implements Runnable {
                 queue.addLast(nextChar); // add next open parentheses
             } else {
                 if (queue.isEmpty()) {
-                    return false;
+                    return false; // wrong sequence
                 }
 
                 char lastChar = queue.peekLast();
