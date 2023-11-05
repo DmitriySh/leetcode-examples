c7158ea83d2f94d07ed477fdb199a9195f6d4167
DmitriySh <dashishmakov@gmail.com>
2023-11-05 16:37:46 +0300

    Edit print shifts

--- src/main/java/ru/shishmakov/problems/NumberOfOneBits.java
+++ src/main/java/ru/shishmakov/problems/NumberOfOneBits.java
@@ -39,6 +39,7 @@ public class NumberOfOneBits implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public static final int DEFAULT_NUMBER = 712;
 
+    private final List<Integer> shifts = new ArrayList<>(32);
     private final int number;
     private int oneBitsCount;
 
@@ -56,22 +57,20 @@ public class NumberOfOneBits implements Runnable {
         logger.info("Number: {}", number);
 
         this.oneBitsCount = countBits();
+        shifts.forEach(n -> logger.info(
+                MessageFormat.format("Shifted number = {0}; bits = {1}", n, Integer.toBinaryString(n))
+        ));
         logger.info("Result. It was counted {} of '1' bits", this.oneBitsCount);
     }
 
     private int countBits() {
         int count = 0;
-        List<Integer> shifts = new ArrayList<>(32);
 
         for (int i = 0; i < 32; i++) {
             int temp = this.number;
             count += (temp = (temp >> i)) & 1;
             shifts.add(temp);
         }
-
-        shifts.forEach(n -> logger.info(
-                MessageFormat.format("Shifted number = {0}; bits = {1}", n, Integer.toBinaryString(n))
-        ));
         return count;
     }
 }
