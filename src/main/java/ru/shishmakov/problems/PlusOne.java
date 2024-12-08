0175c28e0d98f4f45487b393e583dbdd492f22e7
DmitriySh <dashishmakov@gmail.com>
2024-12-08 19:41:44 +0300

    Edit PlusOne

--- src/main/java/ru/shishmakov/problems/PlusOne.java
+++ src/main/java/ru/shishmakov/problems/PlusOne.java
@@ -46,6 +46,7 @@ public class PlusOne implements Runnable {
 
     private int[] plusOne(int[] digits) {
         int index = digits.length - 1;
+
         while (index >= 0) {
             digits[index]++;
             digits[index] %= 10;
