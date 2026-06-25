e2b868d19666a6d6484a91834e4b387cbaf134ad
DmitriySh <dashishmakov@gmail.com>
2026-06-25 19:16:32 +0300

    Fix RemoveDuplicatesFromSortedArray

--- src/main/java/ru/shishmakov/problems/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/ru/shishmakov/problems/RemoveDuplicatesFromSortedArray.java
@@ -65,6 +65,16 @@ public class RemoveDuplicatesFromSortedArray implements Runnable {
             return nums.length;
         }
 
+        int left = 0;
+        int right = 1;
+        while (right < nums.length) {
+            if (nums[left] != nums[right]) {
+                left++;
+                nums[left] = nums[right];
+            }
+            right++;
+        }
+
         return left + 1; // count with zero element
     }
 }
