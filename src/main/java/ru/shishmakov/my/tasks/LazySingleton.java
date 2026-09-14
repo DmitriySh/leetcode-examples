67853eeb8db4c84159cb042a164086adde3b5cbb
DmitriySh <dashishmakov@gmail.com>
2026-09-14 14:27:07 +0300

    Add LazySingleton

---
+++ src/main/java/ru/shishmakov/my/tasks/LazySingleton.java
@@ -0,0 +1,29 @@
+package ru.shishmakov.my.tasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+public class LazySingleton {
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private LazySingleton() {
+        // dummy
+    }
+
+    private static class SingletonHolder {
+        private static final LazySingleton HOLDER_INSTANCE = new LazySingleton();
+    }
+
+    public static LazySingleton getInstance() {
+        return SingletonHolder.HOLDER_INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        LazySingleton singleton = LazySingleton.getInstance();
+        LazySingleton otherSingleton = LazySingleton.getInstance();
+
+        System.out.println("singleton == otherSingleton: " + (singleton == otherSingleton));
+    }
+}
